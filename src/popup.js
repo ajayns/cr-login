62daed912c515617919f5645353c0657a21f7650
Ajay NS <ajay.ns@outlook.com>
2017-06-27 17:09:37 +0530

    Add localStorage functionality

--- src/popup.js
+++ src/popup.js
@@ -2,10 +2,16 @@ chrome.tabs.getSelected(null, function (tab) {
 	document.getElementById('currentLink').innerHTML = tab.url;
 });
 
-document.getElementById('submit').onclick=save;
+document.getElementById('submit').onclick = save;
 
 function save() {
 	var un = document.getElementById("un").value;
 	var pwd = document.getElementById("pwd").value;
-	document.getElementById("res").innerHTML = un + pwd;
+
+	localStorage.setItem("username", un);
+	localStorage.setItem("password", pwd);
+	
+	var temp = localStorage.getItem("username");
+	
+	document.getElementById("res").innerHTML = temp;
 }
