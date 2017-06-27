c8773b216f3ea81d4df3045cce19312069ceb905
Ajay NS <ajay.ns@outlook.com>
2017-06-27 17:06:27 +0530

    Add input form

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,11 @@
-   chrome.tabs.getSelected(null, function(tab) {
-      document.getElementById('currentLink').innerHTML = tab.url;
-    });
+chrome.tabs.getSelected(null, function (tab) {
+	document.getElementById('currentLink').innerHTML = tab.url;
+});
+
+document.getElementById('submit').onclick=save;
+
+function save() {
+	var un = document.getElementById("un").value;
+	var pwd = document.getElementById("pwd").value;
+	document.getElementById("res").innerHTML = un + pwd;
+}
