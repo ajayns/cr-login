0b707d92fc04bbae6c534d9bd2562f96eb742fb0
Ajay NS <ajay.ns@outlok.com>
2017-07-26 19:09:20 +0530

    Add test files

--- src/login.js
+++ src/login.js
@@ -2,6 +2,6 @@ chrome.storage.sync.get(['username', 'password'], function (res) {
 	if (res.username != undefined) {
 		document.getElementById('username').value = res.username;
 		document.getElementById('password').value = res.password;
-		document.getElementById('loginSubmit').submit();
+		document.getElementById('logincaption').submit();
 	}
 });
